b50fed9dda28e8b53ea651248a30945a111fdf9f
Dimitri Ferguson <dimitri.ferguson93@gmail.com>
2022-09-12 10:38:02 -0400

    added jwt authentication

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,17 +1,32 @@
 import { createContext, useState } from "react";
 
+export const CurrentUser = createContext();
 
-export const CurrentUser = createContext()
+__;
+___;
+function CurrentUserProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const getLoggedInUser = async () => {
+      let response = await fetch(
+        "http://localhost:5000/authentication/profile",
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      let user = await response.json();
+      setCurrentUser(user);
+    };
+    getLoggedInUser();
+  }, []);
 
-function CurrentUserProvider({ children }){
-
-    const [currentUser, setCurrentUser] = useState(null)
-
-    return (
-        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
-        </CurrentUser.Provider>
-    )
+  return (
+    <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+      {children}
+    </CurrentUser.Provider>
+  );
 }
 
-export default CurrentUserProvider
+export default CurrentUserProvider;
